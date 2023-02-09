c469eb10ca9c57f07588b2799e1d477eb41f61ac
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-02-08 19:24:17 -0700

    working on deployment

--- Assets/script.js
+++ Assets/script.js
@@ -1,88 +1,31 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 $(document).ready(function () {
-  // listen for save button clicks
   $(".saveBtn").on("click", function () {
-    // get nearby values
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    // save in localStorage
     localStorage.setItem(time, value);
   });
   $("#displayCurrentDay").text(dayjs().format("dddd, MMMM Do"));
-
-  $("#displayCurrentDay").html(todayDate);
   $(function () {
+    var currentHour = dayjs().hour();
     var timeBlock = document.getElementsByClassName("time-block");
     for (var i = 0; i < timeBlock.length; i++) {
-      if (currentHour == currentHour) {
-        document.body.style.background = "past";
+      if (currentHour == Number.parseInt(timeBlock[i].getAttribute('id'))) {
+        timeBlock[i].classList.add("past");
       } else {
-        document.body.style.background = "present";
+        timeBlock[i].classList.add("present");
       }
     }
     function hourUpdater() {
-      // get current number of hours
-      var currentHour = moment().hours();
-      // TODO: Add a listener for click events on the save button. This code should
-      // use the id in the containing time-block as a key to save the user input in
-      // local storage. HINT: What does `this` reference in the click listener
-      // function? How can DOM traversal be used to get the "hour-x" id of the
-      // time-block containing the button that was clicked? How might the id be
-      // useful when saving the description in local storage?
-      //
-      // TODO: Add code to apply the past, present, or future class to each time
-      // block by comparing the id to the current hour. HINTS: How can the id
-      // attribute of each time-block be used to conditionally add or remove the
-      // past, present, and future classes? How can Day.js be used to get the
-      // current hour in 24-hour time?
-      //
-      // TODO: Add code to get any user input that was saved in localStorage and set
-      // the values of the corresponding textarea elements. HINT: How can the id
-      // attribute of each time-block be used to do this?
-      //
-      // TODO: Add code to display the current date in the header of the page.
     };
-    const date = new Date();
-    let hours = date.getHours();
-    const currentHour = hours;
-    console.log(currentHour);
-
-    let ampm = 'AM';
-    if (hours >= 12) {
-      ampm = 'PM';
-    }
-
-    hours = hours % 12;
-    let counter = 0; // 1 2
-
-    for (x = 0; x < 9; x++) {
-
-
-      if (hours + counter == 12) {
-        ampm = "AM";
-      }
-
-      if (hours + counter > 12) {
-        hours = 1;
-        counter = 0;
-      }
-
-      console.log(hours + counter, ampm);
-      counter++;
-    }
-
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
-    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
-    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
-    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
-    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
-  })});
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+  })
+});
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # My-Work-Day-Scheduler
-A simple calendar application that allows a user to save events for each hour of the day.
+A simple calendar application that allows a user to save events for each hour of the common workday.
+
+## Deployed Application
+
+View at: https://tiffany-brand.github.io/work-day-timeblock-planner/
+
+## Table of Contents
+
+1. [Description](#Description)
+2. [Technologies](#technologies)
+3. [Requirements](#Requirements)
+4. [Screenshots](#Screenshots)
+5. [Contact](#Contact)
+6. [License](#License)
+
+
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+- Bootstrap - https://getbootstrap.com/
+- Moment JS - https://momentjs.com/ 
+
+## Requirements
+
+### User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+### Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Screenshots
+
+### Desktop
+![CalendarScreenShotBg](https://user-images.githubusercontent.com/16748389/88445715-8677cf80-cdf2-11ea-9f7b-ed5d021edf01.JPG)
+
+### Mobile
+![mobile-calendar](https://user-images.githubusercontent.com/16748389/90188622-d01c6000-dd89-11ea-8315-132d78ff4440.JPG)
+
+## Contact
+
+- [Tiffany Brand](https://github.com/tiffany-brand)
+
+## License
+
+[MIT License](./LICENSE)
